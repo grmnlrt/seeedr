5ed62ae4d49f4a5038e36ca3b232423c435eb1d8
k-boiteau <boiteau.kev@gmail.com>
2017-05-30 16:08:32 +0200

    Pundit set up

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
   root to: 'pages#home'
+  resources :artworks, only: [:show, :new, :create, :edit, :update, :destroy]
+  # resources :users, only: [:show, :edit, :update, :destroy]
+
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
