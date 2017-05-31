2b8901efc664cb0a5c3fda5346ad20f69e617124
k-boiteau <boiteau.kev@gmail.com>
2017-05-31 17:32:59 +0200

    Dry du datepicker.js

--- app/assets/javascripts/datepicker.js
+++ app/assets/javascripts/datepicker.js
@@ -2,16 +2,4 @@ $(document).ready(function(){
   $('#datetimepicker1').datetimepicker({
     format: "YYYY/MM/DD"
   });
-  $(function () {
-    $('#datetimepicker6').datetimepicker();
-    $('#datetimepicker7').datetimepicker({
-        useCurrent: false //Important! See issue #1075
-    });
-    $("#datetimepicker6").on("dp.change", function (e) {
-        $('#datetimepicker7').data("DateTimePicker").minDate(e.date);
-    });
-    $("#datetimepicker7").on("dp.change", function (e) {
-        $('#datetimepicker6').data("DateTimePicker").maxDate(e.date);
-    });
-  });
 });
