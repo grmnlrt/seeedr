f8dc7a8fdab3f978a01174aa973145217c525dad
Alexis Guinebretiere <alexis.guineb@gmail.com>
2017-05-31 12:08:46 +0200

    RailsAdmin updated

--- config/initializers/rails_admin.rb
+++ config/initializers/rails_admin.rb
@@ -1,5 +1,9 @@
 RailsAdmin.config do |config|
 
+  config.authorize_with do |controller|
+    redirect_to main_app.root_path unless current_user && current_user.admin
+  end
+
 
 
   ### Popular gems integration
