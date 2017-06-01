70e4597129ba342b5e087ca920d84648d51447a6
k-boiteau <boiteau.kev@gmail.com>
2017-06-01 14:00:32 +0200

    after Flo coreections

--- app/assets/javascripts/google_maps_autocomplete.js
+++ app/assets/javascripts/google_maps_autocomplete.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
-  var flat_address = $('#exhibition_address').get(0);
+  var exhibition_address = $('#exhibition_address').get(0);
 
-  if (flat_address) {
+  if (exhibition_address) {
     var autocomplete = new google.maps.places.Autocomplete(exhibition_address, { types: ['geocode'] });
     google.maps.event.addDomListener(flat_address, 'keydown', function(e) {
       if (e.keyCode == 13) {
