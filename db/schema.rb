295fdfe2de9b5e62a5ef3304a59b9b558e6f952b
k-boiteau <boiteau.kev@gmail.com>
2017-06-01 10:21:09 +0200

    correction

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 20170531160706) do
-
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -91,9 +89,9 @@ ActiveRecord::Schema.define(version: 20170531160706) do
     t.integer  "min_price"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.integer  "duration"
     t.float    "latitude"
     t.float    "longitude"
-    t.integer  "duration"
     t.index ["user_id"], name: "index_exhibitions_on_user_id", using: :btree
   end
 
