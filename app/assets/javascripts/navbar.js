e2f5d05252f504cefe291d8bd6f2eed172b05e5c
Alexis Guinebretiere <alexis.guineb@gmail.com>
2017-06-02 09:48:42 +0200

    fixed navbar stick to the top

--- app/assets/javascripts/navbar.js
+++ app/assets/javascripts/navbar.js
@@ -1,15 +1,14 @@
-$(document).ready(function() {
-
-  $(window).scroll(function () {
-      //if you hard code, then use console
-      //.log to determine when you want the
-      //nav bar to stick.
-      console.log($(window).scrollTop())
-    if ($(window).scrollTop() > 280) {
-      $('.navbar-wagon').addClass('navbar-fixed');
-    }
-    if ($(window).scrollTop() < 281) {
-      $('#nav_bar').removeClass('navbar-fixed');
-    }
-  });
-});
+(function($){
+    $(document).ready(function(){
+        var offset = $(".navbar-wagon").offset().top;
+        $(document).scroll(function(){
+            var scrollTop = $(document).scrollTop();
+            if(scrollTop > offset){
+                $(".navbar-wagon").css("position", "fixed");
+            }
+            else {
+                $(".navbar-wagon").css("position", "static");
+            }
+        });
+    });
+})(jQuery);
