b373efa6d6cabf539b0db7582b79e125a1752d23
Alexis Guinebretiere <alexis.guineb@gmail.com>
2017-06-02 11:03:20 +0200

    corrections

--- app/assets/javascripts/navbar.js
+++ app/assets/javascripts/navbar.js
@@ -1,14 +1,14 @@
 (function($){
-    $(document).ready(function(){
-        var offset = $(".navbar-wagon-fixed").offset().top;
-        $(document).scroll(function(){
-            var scrollTop = $(document).scrollTop();
-            if(scrollTop > offset){
-                $(".navbar-wagon-fixed").css("position", "fixed");
-            }
-            else {
-                $(".navbar-wagon-fixed").css("position", "static");
-            }
-        });
+  $(document).ready(function(){
+    var offset = $(".navbar-wagon-fixed").offset().top;
+    $(document).scroll(function(){
+        var scrollTop = $(document).scrollTop();
+        if(scrollTop > offset){
+          $(".navbar-wagon-fixed").css("position", "fixed");
+        }
+        else {
+          $(".navbar-wagon-fixed").css("position", "static");
+        }
     });
+  });
 })(jQuery);
--- db/schema.rb
+++ db/schema.rb
@@ -89,9 +89,9 @@ ActiveRecord::Schema.define(version: 20170531160706) do
     t.integer  "min_price"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.integer  "duration"
     t.float    "latitude"
     t.float    "longitude"
-    t.integer  "duration"
     t.index ["user_id"], name: "index_exhibitions_on_user_id", using: :btree
   end
 
