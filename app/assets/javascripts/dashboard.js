040fe4ebc4fb3f60ebfb4af1a0a46ecf7ead2928
florestan lefevre <florestanlefevre@gmail.com>
2017-06-03 18:16:59 +0200

    replaced buttons by icons

--- app/assets/javascripts/dashboard.js
+++ app/assets/javascripts/dashboard.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
   $('.exhibition-bids').hide();
-  $('.view-bids').click(function(){
+  $('.btn-new-small').click(function(){
     $(this).parents(".exhibition").find('.exhibition-bids').slideToggle();
   });
 });
